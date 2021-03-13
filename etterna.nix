c2cc61afe03bc2666c631f7d340e2f31acf0bd21
leventea <leventea@tuta.io>
2021-03-13 22:30:05 +0100

    Refactor

--- etterna.nix
+++ etterna.nix
@@ -1,33 +1,34 @@
 # nix-shell etterna.nix
+# export out=$PWD/build
 # genericBuild
 
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation rec {
+with pkgs; stdenv.mkDerivation rec {
   name = "Etterna";
   version = "0.70.3";
 
   hardeningDisable = [ "format" ];
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner  = "etternagame";
     repo   = "etterna";
     rev    = "v${version}";
     sha256 = "1vgvg4x6jja97j95lq58f36g29szzl5xzk200wkjq7yxrskz50bj";
   };
 
-  staticOpenSSL = pkgs.openssl.override { static = true; };
-  propagatedBuildInputs = with pkgs; [ staticOpenSSL
-                                       xorg.libX11
-                                       xorg.libXext
-                                       xorg.libXrandr
-                                       curl
-                                       libGLU
-                                       libpulseaudio
-                                       libogg
-                                       # alsaLib
-                                       # libjack2
-                                     ];
+  staticOpenSSL = openssl.override { static = true; };
+  propagatedBuildInputs = [ staticOpenSSL
+                            xorg.libX11
+                            xorg.libXext
+                            xorg.libXrandr
+                            curl
+                            libGLU
+                            libpulseaudio
+                            libogg
+                            # alsaLib
+                            # libjack2
+                          ];
 
-  nativeBuildInputs = with pkgs; [ cmake gnumake pkg-config ];
+  nativeBuildInputs = [ cmake gnumake pkg-config ];
 }
